8eb77b45d68e1d931c64fe9dbc9ae90893021f86
mermi <manel.rhaiem@liqid.de>
2018-08-03 18:19:34 +0200

    Handle tweets regarding cursor value

--- lib/hammoc/scraper/twitter/loader.ex
+++ lib/hammoc/scraper/twitter/loader.ex
@@ -10,12 +10,22 @@ defmodule Hammoc.Scraper.Twitter.Loader do
 		{:producer, state}
 	end
 
-	def handle_demand(demand, %{profile: profile, cursor: _cursor} = state) when demand > 0 do
-    tweets = ExTwitter.favorites(screen_name: profile, count: 100)
+	def handle_demand(demand, %{profile: profile, cursor: nil} = state) when demand > 0 do
+    ExTwitter.favorites(screen_name: profile, count: 100) |> handle_tweets(state)
 
+  end
+
+	def handle_demand(demand, %{profile: profile, cursor: cursor} = state) when demand > 0 do
+    ExTwitter.favorites(screen_name: profile, count: 100, max_id: cursor) |> handle_tweets(state)
+
+  end
+
+  def handle_tweets(tweets, state) do
     IO.inspect [self(),"I am loading new tweets Fuckers!"]
+    min_id_tweet = tweets |> Enum.min_by(fn e -> e.id end)
+    new_state = state |> Map.put(:cursor, min_id_tweet.id - 1)
+		{:noreply, tweets, new_state}
+  end
 
-		{:noreply, tweets, state}
-	end
 
 end
