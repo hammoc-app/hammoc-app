a18f7a7c2b45e34b99a45174fe035d39736417a0
Arno Dirlam <arnodirlam@googlemail.com>
2019-02-11 17:11:10 +0100

    Format code; CI: Check code format

--- lib/hammoc/scraper/twitter/collector.ex
+++ lib/hammoc/scraper/twitter/collector.ex
@@ -1,23 +1,22 @@
 defmodule Hammoc.Scraper.Twitter.Collector do
-	use GenStage
+  use GenStage
 
-	def start_link(_arg) do
-		GenStage.start_link(__MODULE__, nil)
-	end
+  def start_link(_arg) do
+    GenStage.start_link(__MODULE__, nil)
+  end
 
-	def init(nil) do
-		{:consumer, nil, subscribe_to: [{Hammoc.Scraper.Twitter.Loader, max_demand: 20}]}
-	end
+  def init(nil) do
+    {:consumer, nil, subscribe_to: [{Hammoc.Scraper.Twitter.Loader, max_demand: 20}]}
+  end
 
-	def handle_events(tweets, _from, state) do
-		new_tweets = tweets |> Enum.map(fn e -> e.text end)
-		new_tweets |> Enum.each(fn t -> IO.inspect([self(),t]) end)
+  def handle_events(tweets, _from, state) do
+    new_tweets = tweets |> Enum.map(fn e -> e.text end)
+    new_tweets |> Enum.each(fn t -> IO.inspect([self(), t]) end)
 
-		IO.inspect [self(),"sleeping for 10s"]
-		:timer.sleep(10_000)
-		IO.inspect [self(),"awake"]
+    IO.inspect([self(), "sleeping for 10s"])
+    :timer.sleep(10_000)
+    IO.inspect([self(), "awake"])
 
-		{:noreply, [], state}
-	end
-
+    {:noreply, [], state}
+  end
 end
