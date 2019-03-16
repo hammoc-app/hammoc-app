5344276a990e325d0bc43feeb5e768d83d3d1ef8
Arno Dirlam <arnodirlam@googlemail.com>
2019-03-16 11:27:50 +0100

    Systemd: Make SOCKET_NOTIFY great again (add logs, idle when not needed, ...)

--- src/systemd.erl
+++ src/systemd.erl
@@ -3,49 +3,69 @@
 -module(systemd).
 % This is what you need to adopt systemd in erlang
 
--export([ready/0, reloading/0, stopping/0, watchdog/0]).
+-export([ready/1, reloading/1, stopping/1, watchdog/1]).
 
 -export([start_link/0]).
 -export([init/1, handle_info/2, terminate/2]).
 
+ready(Path) -> call(Path, <<"READY=1">>).
+reloading(Path) -> call(Path, <<"RELOADING=1">>).
+stopping(Path) -> call(Path, <<"STOPPING=1">>).
+watchdog(Path) -> call(Path, <<"WATCHDOG=1">>).
 
-ready() -> call(<<"READY=1">>).
-reloading() -> call(<<"RELOADING=1">>).
-stopping() -> call(<<"STOPPING=1">>).
-watchdog() -> call(<<"WATCHDOG=1">>).
+call(Path, Call) ->
+  case gen_udp:open(0, [local]) of
+    {error, SocketError} ->
+      logger:error("Could not open NOTIFY_SOCKET: ~p", [SocketError]),
+      {error, SocketError};
 
+    {ok, Socket} ->
+      Result = gen_udp:send(Socket, {local, Path}, 0, Call),
+      gen_udp:close(Socket),
+      case Result of
+        {error, SendError} ->
+          logger:error("Could not send via NOTIFY_SOCKET: ~p", [SendError]),
+          Result;
 
-call(Call) ->
-  case os:getenv("NOTIFY_SOCKET") of
-    false ->
-      ok;
-    Path ->
-      case gen_udp:open(0, [local]) of
-        {error, SocketError} ->
-          {error, SocketError};
-        {ok, Socket} ->
-          Result = gen_udp:send(Socket, {local,Path}, 0, Call),
-          gen_udp:close(Socket),
-          Result
+        _ -> Result
       end
   end.
-
 
 start_link() ->
   gen_server:start_link({local,?MODULE}, ?MODULE, [], []).
 
-
 init([]) ->
-  ok = ready(),
-  erlang:send_after(60000, self(), watchdog),
-  {ok, state}.
+  case os:getenv("NOTIFY_SOCKET") of
+    false ->
+      logger:debug("NOTIFY_SOCKET is not set."),
+      {ok, state};
 
+    Path ->
+      logger:info("NOTIFY_SOCKET is ~p. Sending READY=1 message.", [Path]),
+      erlang:send(self(), ready),
+      case os:getenv("WATCHDOG_USEC") of
+        false ->
+          logger:debug("WATCHDOG_USEC is not set."),
+          {ok, {Path, false}};
 
-handle_info(watchdog, State) ->
-  ok = watchdog(),
-  erlang:send_after(60000, self(), watchdog),
+        UsecStr ->
+          {Usec, _} = string:to_integer(UsecStr),
+          MsecHalf = Usec div 2000,
+          logger:info("WATCHDOG_USEC is ~p. Sending WATCHDOG=1 messages every ~ps.", [Usec, MsecHalf div 1000]),
+          erlang:send(self(), watchdog),
+          {ok, {Path, MsecHalf}}
+      end
+  end.
+
+handle_info(ready, State = {Path, _}) ->
+  ok = ready(Path),
+  {noreply, State};
+
+handle_info(watchdog, State = {Path, MsecHalf}) ->
+  erlang:send_after(MsecHalf, self(), watchdog),
+  ok = watchdog(Path),
   {noreply, State}.
 
-terminate(_,_) ->
-  ok = stopping(),
+terminate(_, {Path, _}) ->
+  ok = stopping(Path),
   ok.
