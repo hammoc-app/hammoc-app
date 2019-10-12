5925be8a02e213eee92fe40cbcf7bff3ca051207
Arno Dirlam <arnodirlam@googlemail.com>
2019-10-12 17:58:13 +0200

    Revert docker-compose changes

--- rel/config.exs
+++ rel/config.exs
@@ -8,9 +8,9 @@
 |> Enum.map(&Code.eval_file(&1))
 
 use Distillery.Releases.Config,
-    # This sets the default release built by `mix release`
+    # This sets the default release built by `mix distillery.release`
     default_release: :default,
-    # This sets the default environment used by `mix release`
+    # This sets the default environment used by `mix distillery.release`
     default_environment: Mix.env()
 
 # For a full list of config options for both releases
@@ -44,8 +44,8 @@ end
 
 # You may define one or more releases in this file.
 # If you have not set a default release, or selected one
-# when running `mix release`, the first release in the file
-# will be used by default
+# when running `mix distillery.release`, the first release
+# in the file will be used by default
 
 release :hammoc do
   set version: current_version(:hammoc)
